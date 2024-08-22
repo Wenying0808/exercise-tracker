137b2c7280ade12ac6242124f926a811e49cb147
Wenying Chang <changwenyingnl@gmail.com>
2024-08-22 20:27:54 +0200

    feat:/connect to mongoose

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require('mongoose');
+
+//connect to mongoose
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>{console.log('connected')})
 
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+const userSchema = new mongoose.Schema({
 
+});
 
 
 
