d76144dbd92ad5ef46fed9e9c06daa4b557d5577
Wenying Chang <changwenyingnl@gmail.com>
2024-08-23 15:38:43 +0200

    feat:/ save new user to db

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 //connect to mongoose
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>{console.log('Connected to MongoDB!')})
+.catch(err => console.error('Error connecting to MongoDB:', err));
 
 const userSchema = new mongoose.Schema({
   username: String
@@ -21,11 +22,22 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-app.post('/api/users', (req, res) => {
+app.post('/api/users', async(req, res) => {
   const name = req.body.username;
-  res.json({
-    username: name
-  })
+  
+  try {
+    // create new instance of the user model
+    const newUser = new User({ username: name });
+
+    // save new user to database
+    const savedUser = await newUser.save();
+
+    res.json({
+      username: savedUser.username
+    })
+  } catch (err) {
+    console.error("Error save new user", err)
+  }
 })
 
 
